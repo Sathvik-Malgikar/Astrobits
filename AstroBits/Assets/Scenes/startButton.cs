e1ae75897e811bb56ec0f1e04c3c1ee2285c9ab6
Sathvik-Malgikar <102981556+Sathvik-Malgikar@users.noreply.github.com>
2022-11-27 00:07:59 +0530

    added scenes management

--- AstroBits/Assets/Scenes/startButton.cs
+++ AstroBits/Assets/Scenes/startButton.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement
-public class Start : MonoBehaviour
+using UnityEngine.SceneManagement;
+public class startButton : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void loadGame(){
+Debug.Log("esdghwg");
+
+    SceneManager.LoadScene()
+
     }
 
     // Update is called once per frame
