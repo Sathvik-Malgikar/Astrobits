d84b53c26b0319eb560d52d3d95cb30308c37de7
Sathvik-Malgikar <102981556+Sathvik-Malgikar@users.noreply.github.com>
2022-11-26 19:17:01 +0530

    adding randomenes

---
+++ AstroBits/Assets/randomiser.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomiser : MonoBehaviour
+{
+
+    public float[] poslist = {-1.22,-0.31,0.8,1.76,3.41,5,7.62,8.8};
+
+    void randomiser(){
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
