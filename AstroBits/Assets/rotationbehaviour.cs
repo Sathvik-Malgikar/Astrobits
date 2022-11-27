888f02bc44ff2c3826e3591f63c04a82eca0ee1c
Hemabhushan-r <hemabhushanr3@gmail.com>
2022-11-27 07:41:30 +0530

    Teleportation

--- AstroBits/Assets/rotationbehaviour.cs
+++ AstroBits/Assets/rotationbehaviour.cs
@@ -6,17 +6,23 @@ public class rotationbehaviour : MonoBehaviour
 {
    
         public bool active;
+        public float speed;
     // Start is called before the first frame update
     void Start()
     {
-       
-      
+      active=false;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if(Input.GetKeyDown("p")){
+            active=!active;
+        }
+
+
           if(active)
-        GameObject.Find("planets").GetComponent<Transform>().Rotate(Vector3.down,1f);
+        gameObject.GetComponent<Transform>().Rotate(Vector3.down,speed);
     }
 }
