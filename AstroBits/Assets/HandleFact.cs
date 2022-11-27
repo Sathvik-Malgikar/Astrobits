d5fb421da35d7871d6eceeafcf24fb80da855ac1
Sathvik-Malgikar <102981556+Sathvik-Malgikar@users.noreply.github.com>
2022-11-27 10:38:37 +0530

    added fact timer

--- AstroBits/Assets/HandleFact.cs
+++ AstroBits/Assets/HandleFact.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HandleFact : MonoBehaviour
 {
+    public float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+timer=0f;
+
+
+        gameObject.GetComponent<TextMeshProUGUI>().text = "";
+    }
+
+    void changeText(string s){
+gameObject.GetComponent<TextMeshProUGUI>().text = s;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+          if(timer!=0f){
+            timer-=Time.deltaTime;
+            if(timer<=0){
+            timer=0f;
+               gameObject.GetComponent<TextMeshProUGUI>().text = "";
+            }
+          }
+
+        else if(gameObject.GetComponent<TextMeshProUGUI>().text!="" && timer==0f){
+timer = 2f;
+        }
+      
         
+
+
+
     }
+
 }
