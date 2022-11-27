0bb91e6923c498300e65821aa55bd4fbebe9ee52
Sathvik-Malgikar <102981556+Sathvik-Malgikar@users.noreply.github.com>
2022-11-27 07:31:19 +0530

    measuring lightyears

---
+++ AstroBits/Assets/updatedistance.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class updatedistance : MonoBehaviour
+{
+    public float dist;
+    private GameObject origin;
+    private GameObject mespoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        origin = GameObject.Find("origin");
+        mespoint = GameObject.Find("measuringpoint");
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            dist = (origin.transform.position - mespoint.transform.position).magnitude;
+        gameObject.GetComponent<TextMeshProUGUI>().text = "Distance from Earth in lightyears :"+dist;
+    }
+}
